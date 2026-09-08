536305d8d4f3aac9e9027411c0e25ffe69aa1287
Juniorbor <juniorbor1986@gmail.com>
2026-09-08 09:50:33 -0400

    Remove Radiologia & DICOM menu as requested by user

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { Odontograma } from './components/Odontograma';
 import { RadiografiaViewer } from './components/RadiografiaViewer';
 import { FotografiasGaleria } from './components/FotografiasGaleria';
 import { Producao } from './components/Producao';
-import { PainelRadiologiaMain } from './components/radiologia/PainelRadiologiaMain';
 import { AIAssistant } from './components/AIAssistant';
 import { Financeiro } from './components/Financeiro';
 import { Relatorios } from './components/Relatorios';
@@ -544,13 +543,6 @@ export function App() {
 
           {activeTab === 'producao' && usuarioLogado?.role !== 'cliente' && (
             <Producao darkMode={darkMode} usuarioId={usuarioLogado?.id} />
-          )}
-
-          {activeTab === 'radiologia' && (
-            <PainelRadiologiaMain
-              darkMode={darkMode}
-              pacienteNome={pacienteOdontograma?.nome || 'Paciente Exemplo'}
-            />
           )}
 
           {activeTab === 'odontograma' && (
